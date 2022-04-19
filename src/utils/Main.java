e7d4a0453ea862550b6a82ed60f973b2763001dc
José Luis Rodes Maciel Junior <31461584+juniorrodes@users.noreply.github.com>
2022-04-18 21:39:07 -0300

    Remoção de imports desnecessários

--- src/utils/Main.java
+++ src/utils/Main.java
@@ -1,11 +1,5 @@
 package utils;
 
-import cinema.Room;
-import services.Booking;
-
-import java.util.Locale;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
